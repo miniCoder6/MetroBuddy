83ba8cf4a390a4d67dc27a103d267e8d554a9e46
Shreyansh Kumar <shreyansh280705@gmail.com>
2026-04-18 00:24:16 +0530

    refactor: remove custom telemetry module to avoid data collection overhead

--- frontend/lib/telemetry.ts
+++
@@ -1,5 +0,0 @@
-﻿export const TelemetryLogger = {
-    logPageView: (path: string) => console.log([Telemetry] PageView: ),
-    logInteraction: (elementId: string) => console.log([Telemetry] Click: ),
-    reportVitals: (vitals: any) => console.log([Telemetry] WebVitals:, vitals)
-};
